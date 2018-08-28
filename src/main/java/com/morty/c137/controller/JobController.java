c54f8164bbc0713eacc644958a9f37cb9d8cc34a
Patrick95 <lawcy@qq.com>
2018-08-28 15:48:44 +0800

    添加API结果类 统一响应封装

--- src/main/java/com/morty/c137/controller/JobController.java
+++ src/main/java/com/morty/c137/controller/JobController.java
@@ -2,6 +2,8 @@ package com.morty.c137.controller;
 
 import com.github.pagehelper.PageInfo;
 import com.morty.c137.biz.JobBiz;
+import com.morty.c137.core.Result;
+import com.morty.c137.core.ResultGenerator;
 import com.morty.c137.dto.request.QueryJobReqDto;
 import com.morty.c137.po.Job;
 import org.springframework.web.bind.annotation.*;
@@ -11,53 +13,61 @@ import java.util.Date;
 import java.util.List;
 
 @RestController
-@RequestMapping("/jobs")
+@RequestMapping("/job")
 public class JobController {
 
     @Resource
     private JobBiz jobBiz;
 
 
-
     @GetMapping
-    public PageInfo index() {
+    public Result index() {
         // TODO params load from GET
         int id = 1;
         QueryJobReqDto queryJobReqDto = new QueryJobReqDto();
         queryJobReqDto.setId(id);
         //分页后的数据
-        return jobBiz.listJobPaging(queryJobReqDto);
+        PageInfo pageInfo = jobBiz.listJobPaging(queryJobReqDto);
+        return ResultGenerator.genSuccessResult(pageInfo);
     }
 
-    @RequestMapping(value ="/job/{id}",method = RequestMethod.GET)
-    public Job getById(@PathVariable("id") Integer id){
-        return jobBiz.getJob(id);
+    @GetMapping("/{id}")
+    public Result getById(@PathVariable("id") Integer id){
+        Job job = jobBiz.getJob(id);
+        return ResultGenerator.genSuccessResult(job);
     }
 
-    @GetMapping("/add")
-    public Job add() {
+    @PostMapping
+    public Result add(@RequestParam("name") String name, @RequestParam("desc") String desc) {
         Job job = new Job();
-        job.setName("name");
-        job.setDesc("desc");
+        job.setName(name);
+        job.setDesc(desc);
         job.setUpdateTime(new Date());
         job.setCreateTime(new Date());
-        jobBiz.saveJob(job);
-        return job;
+        if (jobBiz.saveJob(job)) {
+            return ResultGenerator.genSuccessResult();
+        }
+        return ResultGenerator.genFailResult("添加失败");
     }
 
-    @PostMapping("/update")
-    public Job update(@RequestParam("id") Integer id, @RequestParam("name") String name,
+    @PutMapping("/{id}")
+    public Result update(@PathVariable("id") Integer id, @RequestParam("name") String name,
                          @RequestParam("desc") String desc) {
         Job job = jobBiz.getJob(id);
         job.setName(name);
         job.setDesc(desc);
         job.setUpdateTime(new Date());
-        jobBiz.updateJob(job);
-        return job;
+        if (jobBiz.updateJob(job)) {
+            return ResultGenerator.genSuccessResult();
+        }
+        return ResultGenerator.genFailResult("更新失败");
     }
 
-    @DeleteMapping("/delete/{id}")
-    public Boolean delete(@PathVariable("id") int id) {
-        return jobBiz.deleteJob(id);
+    @DeleteMapping("/{id}")
+    public Result delete(@PathVariable("id") int id) {
+        if (jobBiz.deleteJob(id)) {
+            return ResultGenerator.genSuccessResult();
+        }
+        return ResultGenerator.genFailResult("删除失败");
     }
 }
